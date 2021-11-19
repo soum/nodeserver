0c25d6dc769bfd0701e3bad20046605b7dc2a78b
soum <somugus@gmail.com>
2021-11-19 15:07:34 -0500

    Add files via upload

---
+++ products.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 001,
+        type: 'soap'
+    },
+    {
+        id: 002,
+        type: 'tooth paste'
+    },
+    {
+        id: 003,
+        type: 'donuts'
+    }
+]
